32d5908163e66e57a8d2c3f05792a633e01825e1
Lior <gofmanlior@gmail.com>
2022-12-16 00:43:32 +0200

    adding jobs to job reducer

--- src/client/first-step-client/src/reducers/jobs.js
+++ src/client/first-step-client/src/reducers/jobs.js
@@ -1,5 +1,5 @@
 import {
-    NEW_JOB,JOB_ERROR
+    NEW_JOB,JOB_ERROR, LOAD_JOBS
 } from "../actions/types";
 
 const initialState = {
@@ -18,6 +18,8 @@ export default function (state = initialState, action) {
             return {...state,jobs, loading: false}
         case JOB_ERROR:
             return {...state, errors: payload, loading: false}
+        case LOAD_JOBS:
+            return {...state, jobs: payload, loading: false}
         default:
             return {...state, loading: false};
     }
