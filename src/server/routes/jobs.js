1e5b17552ac2167a8ba71a64c6ced8ab2b802356
nir kanimach <nironi1998@gmail.com>
2022-12-16 00:47:54 +0200

    added basic dashboard ui
    showing all job offers for employers

--- src/server/routes/jobs.js
+++ src/server/routes/jobs.js
@@ -53,15 +53,78 @@ router.post('/', [
                 requiredSkills,
                 jobType
             }
-             if (picture){
-                 jobFields.picture = picture;
-             }
-            if (requiredDays){
+            if (picture) {
+                jobFields.picture = picture;
+            }
+            if (requiredDays) {
                 jobFields.requiredDays = requiredDays;
             }
-             const job = new Job(jobFields);
-             await job.save();
-             res.json({job});
+            const job = new Job(jobFields);
+            await job.save();
+            res.json({job});
+        } catch (e) {
+            console.error(e.message)
+            res.status(500).send('server error')
+        }
+    })
+
+/**
+ *@route    PUT api/jobs/:id
+ *@desc     add a new job offer
+ *@access   Public
+ */
+router.put('/:id', [
+        check('jobTitle', 'please include a job name').exists(),
+        check('business', 'please include a business name').exists(),
+        check('description', 'please include a job description').exists(),
+        check('percentageOfJob', 'please include a percentage of job').exists(),
+        check('location', 'please include a job location').exists(),
+        check('requiredSkills', 'please include the required skills for the job').exists(),
+        check('jobType', 'please include type of the job').exists(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()})
+        }
+        const {
+            jobTitle,
+            business,
+            description,
+            percentageOfJob,
+            profile,
+            location,
+            requiredSkills,
+            jobType,
+            picture,
+            requiredDays
+        } = req.body;
+        try {
+            let employer = await Employer.findOne({_id: profile});
+            if (!employer) {
+                return res.status(404).json({
+                    errors: [{msg: 'No employer found'}]
+                })
+            }
+            const jobFields = {
+                profile,
+                jobTitle,
+                business,
+                description,
+                percentageOfJob,
+                location,
+                requiredSkills,
+                jobType
+            }
+            if (picture) {
+                jobFields.picture = picture;
+            }
+            if (requiredDays) {
+                jobFields.requiredDays = requiredDays;
+            }
+            const job = new Job(jobFields);
+            await job.save();
+            res.json({job});
         } catch (e) {
             console.error(e.message)
             res.status(500).send('server error')
