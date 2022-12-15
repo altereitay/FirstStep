9e6480cb32f12501a012214496de1ea943a81efb
Lior <gofmanlior@gmail.com>
2022-12-15 20:16:41 +0200

    added ui for for upload a new job

--- src/client/first-step-client/src/actions/types.js
+++ src/client/first-step-client/src/actions/types.js
@@ -9,5 +9,8 @@ export const NEW_EMPLOYER_PROFILE = 'NEW_EMPLOYER_PROFILE'
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const LOGIN_SUCCESS="LOGIN_SUCCESS";
 export const LOGIN_FAIL="LOGIN_FAIL";
-export const PROFILE_LOADED="PROFILE_LOADED"
+export const PROFILE_LOADED="PROFILE_LOADED";
+export const NEW_JOB="NEW_JOB";
+export const JOB_ERROR = 'JOB_ERROR';
+
 
