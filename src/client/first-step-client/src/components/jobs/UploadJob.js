418d738e70d1aa7fed1e7f2d1cefc970932e368b
Lior <gofmanlior@gmail.com>
2022-12-15 20:25:05 +0200

    added on submit

--- src/client/first-step-client/src/components/jobs/UploadJob.js
+++ src/client/first-step-client/src/components/jobs/UploadJob.js
@@ -47,122 +47,124 @@ const UploadJob = ({profile, newJob}) => {
     }
 
     return (
-        <Fragment>
-            <h1 className="large text-primary">Upload Job</h1>
-            <div className='form-group'>
-                <h3 style={{color: '#38a1f3'}}>Job Title</h3>
-                <input type='text'
-                       placeholder='Enter Job Title'
-                       name='jobTitle'
-                       value={jobTitle}
-                       onChange={event => onChange(event)}
-                       required/>
-            </div>
-            <div className='form-group'>
-                <h3 style={{color: '#38a1f3'}}>Business</h3>
-                <input type='text'
-                       placeholder='Enter Business Title'
-                       name='business'
-                       value={business}
-                       onChange={event => onChange(event)}
-                       required/>
-            </div>
-            <div className='form-group'>
-                <h3 style={{color: '#38a1f3'}}>Description</h3>
-                <input type='text'
-                       placeholder='Enter Description'
-                       name='description'
-                       value={description}
-                       onChange={event => onChange(event)}
-                       required/>
-            </div>
-            <div className='form-group'>
-                <h3 style={{color: '#38a1f3'}}>Job Percent</h3>
-                <input type='text'
-                       placeholder='Enter Job Percent'
-                       name='jobPercent'
-                       value={jobPercent}
-                       onChange={event => onChange(event)}
-                       required/>
-            </div>
-            <div className='form-group'>
-                <h3 style={{color: '#38a1f3'}}>location</h3>
-                <input type='text'
-                       placeholder='Enter Job Location'
-                       name='location'
-                       value={location}
-                       onChange={event => onChange(event)}
-                       required/>
-            </div>
-            <div className="form-group">
-                <h3 style={{color: '#38a1f3'}}>Required Skills</h3>
-                <input type="text"
-                       placeholder="Enter Required Skills"
-                       name="requiredSkills"
-                       value={requiredSkills}
-                       onChange={(e) => skillsOnChange(e)}/>
-            </div>
-            <div className="form-group">
-                <h3 style={{color: '#38a1f3'}}>Required Days</h3>
-                <label className='my-1'>
-                    <input type="checkbox"
-                           name="sunday "
-                           className='my-1'
-                           value={sunday}
-                           onChange={(e) => availabilityOnChange(e)}/>
-                    Sunday
-                </label>
-                <label className='my-1'>
-                    <input type="checkbox"
-                           name="monday"
-                           className='my-1'
-                           value={monday}
-                           onChange={(e) => availabilityOnChange(e)}/>
-                    Monday
-                </label>
-                <label className='my-1'>
-                    <input type="checkbox"
-                           name="tuesday"
-                           className='my-1'
-                           value={tuesday}
-                           onChange={(e) => availabilityOnChange(e)}/>
-                    Tuesday
-                </label>
-                <label className='my-1'>
-                    <input type="checkbox"
-                           name="wednesday"
-                           className='my-1'
-                           value={wednesday}
-                           onChange={(e) => availabilityOnChange(e)}/>
-                    Wednesday
-                </label>
-                <label className='my-1'>
-                    <input type="checkbox"
-                           name="thursday"
-                           className='my-1'
-                           value={thursday}
-                           onChange={(e) => availabilityOnChange(e)}/>
-                    Thursday
-                </label>
-                <label className='my-1'>
-                    <input type="checkbox"
-                           name="friday"
-                           className='my-1'
-                           value={friday}
-                           onChange={(e) => availabilityOnChange(e)}/>
-                    Friday
-                </label>
-                <label className='my-1'>
-                    <input type="checkbox"
-                           name="saturday"
-                           className='my-1'
-                           value={saturday}
-                           onChange={(e) => availabilityOnChange(e)}/>
-                    Saturday
-                </label>
-            </div>
-            <input type='submit' className='btn btn-primary my-1'/>
-        </Fragment>
+        <form onSubmit={e =>{onSubmit(e)}}>
+            <Fragment>
+                <h1 className="large text-primary">Upload Job</h1>
+                <div className='form-group'>
+                    <h3 style={{color: '#38a1f3'}}>Job Title</h3>
+                    <input type='text'
+                           placeholder='Enter Job Title'
+                           name='jobTitle'
+                           value={jobTitle}
+                           onChange={event => onChange(event)}
+                           required/>
+                </div>
+                <div className='form-group'>
+                    <h3 style={{color: '#38a1f3'}}>Business</h3>
+                    <input type='text'
+                           placeholder='Enter Business Title'
+                           name='business'
+                           value={business}
+                           onChange={event => onChange(event)}
+                           required/>
+                </div>
+                <div className='form-group'>
+                    <h3 style={{color: '#38a1f3'}}>Description</h3>
+                    <input type='text'
+                           placeholder='Enter Description'
+                           name='description'
+                           value={description}
+                           onChange={event => onChange(event)}
+                           required/>
+                </div>
+                <div className='form-group'>
+                    <h3 style={{color: '#38a1f3'}}>Job Percent</h3>
+                    <input type='text'
+                           placeholder='Enter Job Percent'
+                           name='jobPercent'
+                           value={jobPercent}
+                           onChange={event => onChange(event)}
+                           required/>
+                </div>
+                <div className='form-group'>
+                    <h3 style={{color: '#38a1f3'}}>location</h3>
+                    <input type='text'
+                           placeholder='Enter Job Location'
+                           name='location'
+                           value={location}
+                           onChange={event => onChange(event)}
+                           required/>
+                </div>
+                <div className="form-group">
+                    <h3 style={{color: '#38a1f3'}}>Required Skills</h3>
+                    <input type="text"
+                           placeholder="Enter Required Skills"
+                           name="requiredSkills"
+                           value={requiredSkills}
+                           onChange={(e) => skillsOnChange(e)}/>
+                </div>
+                <div className="form-group">
+                    <h3 style={{color: '#38a1f3'}}>Required Days</h3>
+                    <label className='my-1'>
+                        <input type="checkbox"
+                               name="sunday "
+                               className='my-1'
+                               value={sunday}
+                               onChange={(e) => availabilityOnChange(e)}/>
+                        Sunday
+                    </label>
+                    <label className='my-1'>
+                        <input type="checkbox"
+                               name="monday"
+                               className='my-1'
+                               value={monday}
+                               onChange={(e) => availabilityOnChange(e)}/>
+                        Monday
+                    </label>
+                    <label className='my-1'>
+                        <input type="checkbox"
+                               name="tuesday"
+                               className='my-1'
+                               value={tuesday}
+                               onChange={(e) => availabilityOnChange(e)}/>
+                        Tuesday
+                    </label>
+                    <label className='my-1'>
+                        <input type="checkbox"
+                               name="wednesday"
+                               className='my-1'
+                               value={wednesday}
+                               onChange={(e) => availabilityOnChange(e)}/>
+                        Wednesday
+                    </label>
+                    <label className='my-1'>
+                        <input type="checkbox"
+                               name="thursday"
+                               className='my-1'
+                               value={thursday}
+                               onChange={(e) => availabilityOnChange(e)}/>
+                        Thursday
+                    </label>
+                    <label className='my-1'>
+                        <input type="checkbox"
+                               name="friday"
+                               className='my-1'
+                               value={friday}
+                               onChange={(e) => availabilityOnChange(e)}/>
+                        Friday
+                    </label>
+                    <label className='my-1'>
+                        <input type="checkbox"
+                               name="saturday"
+                               className='my-1'
+                               value={saturday}
+                               onChange={(e) => availabilityOnChange(e)}/>
+                        Saturday
+                    </label>
+                </div>
+                <input type='submit' className='btn btn-primary my-1'/>
+            </Fragment>
+        </form>
     )
 }
 const mapStateToProps = state => ({
