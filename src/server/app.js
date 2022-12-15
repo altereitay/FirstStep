0c62483d2eeacc4a82e83ac417c481c4cf4e6b22
eitay <eitay@soos.org.il>
2022-12-15 18:39:18 +0200

    added backend functionally for upload a new job

--- src/server/app.js
+++ src/server/app.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/profiles', require('./routes/profiles'))
+app.use('/api/jobs', require('./routes/jobs'))
 
 
 module.exports = app;
--- src/server/modules/JobOffer.js
+++ src/server/modules/JobOffer.js
@@ -34,6 +34,36 @@ const JobOffer = new mongoose.Schema({
         type: [String],
         required:true
     },
+    requiredDays: {
+        sunday:{
+            type:Boolean ,
+            default: false
+        },
+        monday:{
+            type:Boolean ,
+            default: false
+        },
+        tuesday:{
+            type:Boolean ,
+            default: false
+        },
+        wednesday:{
+            type:Boolean ,
+            default: false
+        },
+        thursday:{
+            type:Boolean ,
+            default: false
+        },
+        friday:{
+            type:Boolean ,
+            default: false
+        },
+        saturday:{
+            type:Boolean ,
+            default: false
+        }
+    },
     appliedStudents:{
         type:[mongoose.Schema.Types.ObjectId]
     },
