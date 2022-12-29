0be988ffbac9d1c1bc80c12735230b25311478f5
eitay <eitay@soos.org.il>
2022-12-30 01:27:14 +0200

    delete file

--- src/server/tests/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset:'jest',
-    testMatch:['**/**/*.test.js'],
-    verbose: true,
-    forceExit: true,
-    clearMocks: true,
-    resetMocks: true,
-    restoreMocks: true
-}
