1e483c8ce15ca06073068530e66c49a3516bf101
eitay <eitay@soos.org.il>
2022-12-31 23:54:04 +0200

    refactor user test and added most some profile tests

--- src/server/tests/profiles.test.js
+++ src/server/tests/profiles.test.js
@@ -1,50 +1,179 @@
 const request = require('supertest');
 const app = require('../app');
 jest.mock('../modules/EmployerProfile');
-const Employer = require('../modules/EmployerProfile');
+const Employer = require('../modules/EmployerProfile')
+jest.mock('../modules/StudentProfile');
+const Student = require('../modules/StudentProfile')
 const jwt = require("jsonwebtoken");
 
+const employerObject = {
+    "_id": "639b6582fa63e7290b2b1e92",
+    "user": "639b6554fa63e7290b2b1e8b",
+    "name": "test Tester",
+    "business": "test business",
+    "picture": "",
+    "description": "mock data for employer checks",
+    "isApproved": false
+}
 
-test('GET api/profiles/:id without auth token', async () => {
-    const response = await request(app)
-        .get('/api/profiles/1')
+const studentObject = {
+    "_id": "638f6bd5aaac83c3ff2cd8ff",
+    "user": "638f571e13c8855f0798262d",
+    "name": "test Tester",
+    "dateOfBirth": "1664928000000",
+    "city": "tel aviv",
+    "education": [{
+        "school": "SCE",
+        "degree": "Software engineering ",
+        "from": "1664928000000",
+        "to": "1671062400000",
+        "current": true,
+        "_id": "638f6bd5aaac83c3ff2cd900"
+    }],
+    "skills": ["js"],
+    "description": "delete",
+    "availability": [{
+        "sunday": true,
+        "monday": true,
+        "tuesday": false,
+        "wednesday": false,
+        "thursday": false,
+        "friday": false,
+        "saturday": false,
+        "_id": "638f6bd5aaac83c3ff2cd901"
+    }],
+    "isApproved": false
+}
 
-    expect(response.statusCode).toBe(401);
-});
+describe('GET api/profiles/:id', () => {
+    test('without auth token', async () => {
+        const response = await request(app)
+            .get('/api/profiles/1')
 
-
-test('GET api/profiles/:id', async () => {
-    Employer.mockImplementation(() => {
-        return {
-            findOne: jest.fn()
-        };
+        expect(response.statusCode).toBe(401);
     });
 
-    const obj = {
-        "_id":"639b6582fa63e7290b2b1e92",
-        "user":"639b6554fa63e7290b2b1e8b",
-        "name":"test Tester",
-        "business":"test business",
-        "picture":"",
-        "description":"mock data for employer checks",
-        "isApproved":false
-    }
-    Employer.findOne.mockReturnValue(obj)
-    const userId = '639b6554fa63e7290b2b1e8b';
 
-    const payload = {
-        user: {
-            id: userId
+    test('with token and good id', async () => {
+        Employer.mockImplementation(() => {
+            return {
+                findOne: jest.fn().mockResolvedValue(employerObject),
+                find: jest.fn().mockResolvedValue(true)
+            };
+        });
+        Employer.find.mockReturnValue(true);
+        Student.mockImplementation(() => {
+            return {
+                exists: jest.fn().mockResolvedValue(false)
+            };
+        });
+
+
+        Employer.findOne.mockReturnValue(employerObject)
+        const userId = '639b6554fa63e7290b2b1e8b';
+        // const userId = '1';
+
+        const payload = {
+            user: {
+                id: userId
+            }
         }
-    }
-    const tok = jwt.sign(payload, require('../configs/defualt.json').JWT_SECRET, {expiresIn: 360000})
+        const tok = jwt.sign(payload, require('../configs/defualt.json').JWT_SECRET, {expiresIn: 360000})
 
-    const response = await request(app)
-        .get(`/api/profiles/${userId}`)
-        .set('x-auth-token', tok)
+        const response = await request(app)
+            .get(`/api/profiles/${userId}`)
+            .set('x-auth-token', tok)
 
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toMatchObject(employerObject);
+    });
+})
 
-    expect(response.statusCode).toBe(200)
-    expect(response).toHaveProperty('headers.content-type', 'application/json');
+describe('POST api/profiles/student', () => {
+    test('empty request', async () => {
+        const response = await request(app).post('/api/profiles/student')
 
-});
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+    })
+    test('user already have profile', async ()=>{
+        Student.mockImplementation(() => {
+            return {
+                exists: jest.fn().mockReturnValue(true)
+            };
+        });
+        Student.exists.mockReturnValue(true);
+        const response = await request(app).post('/api/profiles/student').send(studentObject);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body.msg).toBe('user already have a profile')
+    })
+
+    test('a good request', async ()=>{
+        Student.mockImplementation(() => {
+            return {
+                exists: jest.fn(),
+                save: jest.fn().mockResolvedValue(true)
+            };
+        });
+        Student.exists.mockReturnValue(false);
+
+        Employer.mockImplementation(() => {
+            return {
+                exists: jest.fn()
+            };
+        });
+        Employer.exists.mockReturnValue(false);
+
+        const spy = jest.spyOn(Student, 'constructor');
+        spy.mockResolvedValue(new Student(studentObject));
+        const response = await request(app).post('/api/profiles/student').send(studentObject);
+
+        expect(response.statusCode).toBe(200);
+        spy.mockReset();
+    })
+})
+
+describe('PUT api/profiles/student/:id', ()=>{
+    afterEach(()=>{
+        jest.restoreAllMocks()
+    })
+
+    test('an empty request', async ()=>{
+        const response = await request(app).put('/api/profiles/student/1').send()
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+    })
+
+    test('profile doesnt exists', async ()=>{
+
+        Student.mockImplementation(() => {
+            return {
+                exists: jest.fn()
+            };
+        });
+        Student.exists.mockReturnValue(false);
+        const response = await request(app).put('/api/profiles/student/1').send(studentObject)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toMatchObject({msg: 'profile dosent exist'});
+    })
+
+    test('a good request', async ()=>{
+        Student.mockImplementation(() => {
+            return {
+                exists: jest.fn(),
+                findOneAndUpdate: jest.fn()
+            };
+        });
+        Student.exists.mockReturnValue(true);
+        Student.findOneAndUpdate.mockReturnValue({profile:studentObject, save:jest.fn()});
+
+        const response = await request(app).put(`/api/profiles/student/${studentObject.user}`).send({...studentObject, name: 'update'})
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.profile.profile).toMatchObject(studentObject);
+
+    })
+
+})
--- src/server/tests/users.test.js
+++ src/server/tests/users.test.js
@@ -4,44 +4,48 @@ jest.mock('../modules/User');
 
 const User = require('../modules/User');
 
-test('POST /api/users', async () => {
-    User.mockImplementation(() => {
-        return {
-            save: jest.fn().mockResolvedValue(true)
-        };
+
+describe('POST /api/users', ()=>{
+    test('a new user', async () => {
+        User.mockImplementation(() => {
+            return {
+                save: jest.fn().mockResolvedValue(true)
+            };
+        });
+
+        const validEmail = 'valid@email.com';
+        const validPassword = 'password';
+        const validTypeOfUser = 'student';
+
+        const response = await request(app)
+            .post('/api/users')
+            .send({email: validEmail, password: validPassword, typeOfUser: validTypeOfUser})
+            .expect(200);
+
+        expect(response.body).toHaveProperty('token');
+        expect(User).toHaveBeenCalled();
     });
 
-    const validEmail = 'valid@email.com';
-    const validPassword = 'password';
-    const validTypeOfUser = 'student';
+    test('with existing user', async () => {
+        User.mockImplementation(() => {
+            return {
+                findOne: jest.fn(),
+                save: jest.fn().mockResolvedValue(false)
+            };
+        });
+        User.findOne.mockReturnValue(true)
 
-    const response = await request(app)
-        .post('/api/users')
-        .send({email: validEmail, password: validPassword, typeOfUser: validTypeOfUser})
-        .expect(200);
+        const validEmail = 'alter1eitai@gmail.com';
+        const validPassword = '123456';
+        const validTypeOfUser = 'student';
 
-    expect(response.body).toHaveProperty('token');
-    expect(User).toHaveBeenCalled();
-});
+        const response = await request(app)
+            .post('/api/users')
+            .send({email: validEmail, password: validPassword, typeOfUser: validTypeOfUser})
 
-test('POST /api/users with existing user', async () => {
-    User.mockImplementation(() => {
-        return {
-            findOne: jest.fn(),
-            save: jest.fn().mockResolvedValue(false)
-        };
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toEqual([{msg: 'User exist'}]);
     });
-    User.findOne.mockReturnValue(true)
 
-    const validEmail = 'alter1eitai@gmail.com';
-    const validPassword = '123456';
-    const validTypeOfUser = 'student';
-
-    const response = await request(app)
-        .post('/api/users')
-        .send({email: validEmail, password: validPassword, typeOfUser: validTypeOfUser})
-
-    expect(response.statusCode).toBe(400)
-    expect(response.body).toHaveProperty('errors');
-    expect(response.body.errors).toEqual([{msg: 'User exist'}]);
-});
+})
